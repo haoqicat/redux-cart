25dc13c6ba7d4f0033ea71d500c8533c7a8f2c9b
Peter Wang <happypeter1983@gmail.com>
2017-12-28 16:26:41 +0800

    button

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react'
 import store from '../store'
 
 class Products extends Component {
+  handleClick = productId => {
+    store.dispatch({ type: 'ADD_TO_CART', productId })
+  }
+
   render() {
     const { products } = store.getState()
     console.log(store.getState())
-    const productList = products.map(t => <div key={t.id}>{t.name}</div>)
+    const productList = products.map(t => (
+      <div key={t.id}>
+        <div>
+          {t.name}
+        </div>
+        <button onClick={() => this.handleClick(t.id)} >购买</button>
+      </div>)
+    )
     return (
       <div>
         <h2> 所有商品 </h2>
