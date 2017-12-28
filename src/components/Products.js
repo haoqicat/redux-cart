823c08a5eb73f2fe75caf6eca65bc18586adb8fd
Peter Wang <happypeter1983@gmail.com>
2017-12-28 10:59:20 +0800

    product list

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
 
 class Products extends Component {
-  render () {
+  state = {
+    products: [
+      {
+        id: '324',
+        name: '苹果电脑'
+      },
+      {
+        id: '452',
+        name: '橘子'
+      }
+    ]
+  }
+  render() {
+    const { products } = this.state
+    const productList = products.map(t => <div key={t.id}>{t.name}</div>)
     return (
       <div>
-        Products
+        <h2> 所有商品 </h2>
+        {productList}
       </div>
     )
   }
 }
 
-export default  Products
+export default Products
