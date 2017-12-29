515568bc8f7f79be1149ab97596a6d6c9dfb8fde
Peter Wang <happypeter1983@gmail.com>
2017-12-29 10:08:58 +0800

    aviod dup

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -3,6 +3,9 @@ const initialState = []
 const cart = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
+      if(state.indexOf(action.productId) !== -1) {
+        return state
+      }
       return [...state, action.productId]
     default:
       return state
